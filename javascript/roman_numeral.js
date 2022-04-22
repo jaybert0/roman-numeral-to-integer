45a79b96ea81e91a895b5d8d15b8bcfb2f7c415b
J Lee <jay.j.lee.1@gmail.com>
2022-04-22 12:51:14 -0400

    clever

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,35 @@
 function romanNumeral(string) {
-  // type your code here
+  const romans = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000
+  };
+  let total = 0;
+  let idx = 0;
+
+  while (idx < string.length) {
+    const twoChar = string[idx] + (string[idx + 1] || '');
+
+    if (romans[twoChar] !== undefined) {
+      total += romans[twoChar];
+      idx += 2;
+    } else {
+      total += romans[string[idx]];
+      ++idx;
+    }
+  }
+
+  return total;
 }
 
 if (require.main === module) {
@@ -21,4 +51,13 @@ if (require.main === module) {
 module.exports = romanNumeral;
 
 // Please add your pseudocode to this file
+// I = 1; V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+// 1	5	10	50	100	500	1000
+// I
+// V
+// X
+// L
+// C
+// D
+// M
 // And a written explanation of your solution
